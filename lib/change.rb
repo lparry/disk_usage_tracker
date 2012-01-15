fdb44bba23233fa6e21d61fa3636265b1813e35e
Lucas Parry <lucas@envato.com>
2012-01-15 16:38:06 +1100

    prettier output

--- lib/change.rb
+++ lib/change.rb
@@ -9,7 +9,14 @@ class Change
   end
 
   def percentage
-    (size_after - size_before).to_f/size_before
+    increase = (size_after - size_before).to_f/size_before
+    if increase.infinite?
+      increase
+    else
+      (increase * 100).to_i
+
+    end
+
   end
 
   def changed?
@@ -31,6 +38,16 @@ class Change
     else
       0
     end
+  end
+
+  def inspect
+    %(#<#{self.class.name}: "path"=>"#{either["path"]}", "directory?"=>#{either["directory?"]}}, "raw_size": #{raw_size}, "percentage": #{percentage}% >)
+  end
+
+  private
+
+  def either
+    (@version_a || @version_b)
   end
 end
 
